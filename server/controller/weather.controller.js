9fd2078c8707a492b71a58c5221c8f6fca360e35
MrChrisFabian <christopher.mendoza@fiuni.edu.py>
2024-05-11 13:55:52 -0400

    feat: getAlertType added

--- server/controller/weather.controller.js
+++ server/controller/weather.controller.js
@@ -5,19 +5,72 @@ const cityIndexMap = {
     'boqueron': 1,
     'caaguazu': 2
 }
+const dayIndexMap = {
+    'hoy': 0,
+    'domingo': 1,
+    'lunes': 2,
+    'martes': 3
+}
+
+const getCityDayData = (city, day) => {
+    const indexDay = dayIndexMap[day]
+    return (getCityData(city).pronostico_extendido_list[indexDay])
+
+
+}
 const getCityData = (city) => {
 
     const index = cityIndexMap[city]
     return data.departamento_list[index]
 
 }
+
+
+const conditionTemp = (day) => {
+    return (day.main.temp >= 10 && day.main.temp <= 25)
+}
+
+const conditionWeather = (day) => {
+    return (day.clima[0].id > 1 && day.clima[0].id < 5)
+}
+
+const conditionWind = (day) => {
+    return (day.viento.velocidad < 40)
+}
+
+const getAlertType = (day) => {
+    if (conditionWeather(day) && conditionTemp(day) && conditionWind(day)) {
+        return "optimo"
+    }
+    if (conditionWeather(day) && (conditionWind(day) || conditionTemp(day))) {
+        return "precaucion"
+    }
+    if (!conditionWeather(day)) {
+        return "precaucion"
+    }
+    if (!conditionWeather(day) && !conditionTemp(day) && !conditionWind(day)) {
+        return "peligroso"
+    }
+
+}
+
 module.exports = {
     weatherCity: (req, res) => {
         const city = req.params.city
-        res.status(200).send(getCityData(city))
+        res.send(getCityData(city))
     },
     weatherCityExt: (req, res) => {
         const city = req.params.city
         res.status(200).send(getCityData(city).pronostico_extendido_list)
     },
+    weatherGetAlert: (req, res) => {
+        const city = req.params.city
+        const day = req.params.day
+        const dataDay = getCityDayData(city, day)
+        if (dataDay != null) {
+            res.send(getAlertType(dataDay));
+        } else {
+            res.status(404).send('No pudimos encontrar información lo sentimos')
+        }
+    }
 }
--- server/routes/weather.routes.js
+++ server/routes/weather.routes.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const { weatherCity, weatherCityExt } = require('../controller/weather.controller')
-
+const { weatherCity, weatherCityExt, weatherGetAlert } = require("../controller/weather.controller")
 const weatherRouter = express.Router();
 
 weatherRouter.get("/:city", weatherCity)
 weatherRouter.get("/:city/data", weatherCityExt)
-weatherRouter.get("/:city/alert")
+weatherRouter.get("/:city/:day/alert", weatherGetAlert)
 module.exports = weatherRouter
